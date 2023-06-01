6a0b25de0080163ecc57ab32c5c974b058920363
Christophe-Saury <95085659+Christophe-Saury@users.noreply.github.com>
2023-06-01 15:34:26 +0200

    declare a method

--- FetchDataBaseballAPI.py
+++ FetchDataBaseballAPI.py
@@ -1,23 +1,28 @@
-
 import requests
+
+def fetch_data(endpoint_url, params):
+    try:
+        response = requests.get(endpoint_url, params=params)
+        response.raise_for_status()  # Raise an exception if the request was unsuccessful
+        data = response.json()
+        return data
+    except requests.exceptions.RequestException as e:
+        print("Error:", str(e))
 
 # Set up authentication if required
 api_key = "YOUR_API_KEY"
 
 # Set the endpoint URL and parameters
-endpoint_url = "http://lookup-service-prod.mlb.com/json/named.sport_career_hitting.bam?league_list_id='mlb'&game_type='R'&player_id='493316'"
+endpoint_url = "http://lookup-service-prod.mlb.com/json/named.sport_career_hitting.bam"
 player_id = "12345"
-params = {"playerId": player_id, "statType": "stolenBase"}
+params = {"league_list_id": "mlb", "game_type": "R", "player_id": player_id}
 
-# Make the HTTP request
+# Fetch the data
+data = fetch_data(endpoint_url, params)
 
-response = requests.get(endpoint_url)
-
-# Handle the response
-if response.status_code == 200:
-    data = response.json()
-    stolen_bases = data
-    print (stolen_bases)
+if data is not None:
+    stolen_bases = data["sport_career_hitting"]["queryResults"]["row"]
+    print(stolen_bases)
     # Process and analyze the stolen base data as needed
 else:
-    print("Error:", response.status_code)
+    print("Failed to fetch data.")
