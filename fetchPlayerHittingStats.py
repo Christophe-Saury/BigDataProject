45d835de92a596134c709746b79266fe9b0cb21f
Christophe-Saury <95085659+Christophe-Saury@users.noreply.github.com>
2023-06-10 12:18:57 +0200

    Updated fetchPlayerHittingStats

--- fetchPlayerHittingStats.py
+++ fetchPlayerHittingStats.py
@@ -1,26 +1,24 @@
 import requests
 
-def fetch_data_API1(endpoint_url, params=None):
+def fetch_data_API1(endpoint_url):
     try:
-        response = requests.get(endpoint_url, params=params)
+        response = requests.get(endpoint_url)
         response.raise_for_status()  # Raise an exception if the request was unsuccessful
         data = response.json()
         return data
     except requests.exceptions.RequestException as e:
         print("Error:", str(e))
 
-# Set the endpoint URL
-endpoint_url = "http://lookup-service-prod.mlb.com/json/named.sport_career_hitting.bam"
-
 # Set the parameters
-params = {
-    "league_list_id": "mlb",
-    "game_type": "R",
-    "player_id": "493316"
-}
+game_type = "'R'"
+season = "'2017'"
+player_id = "'493316'"
+
+# Construct the endpoint URL
+endpoint_url = f"http://lookup-service-prod.mlb.com/json/named.sport_hitting_tm.bam?league_list_id='mlb'&game_type={game_type}&season={season}&player_id={player_id}"
 
 # Fetch the data
-data = fetch_data_API1(endpoint_url, params=params)
+data = fetch_data_API1(endpoint_url)
 
 # Print the data
 print(data)
